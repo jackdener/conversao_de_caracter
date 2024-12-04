b6aa0bb70ef5a57817dc632c90b102d8289ae6b9
professor <professor@SN-371156.edu.fiesc.com.br>
2024-12-04 20:58:08 -0300

    criação da pagina, colocando funcionalidade de conversão de caracteres

---
+++ script.js
@@ -0,0 +1,10 @@
+let campoTexto = document.getElementById("texto");
+function paraMaiusculo(){
+    let texto = campoTexto.value; 
+    campoTexto.value = texto.toUpperCase();
+}
+
+function paraMinusculo(){
+    let texto = campoTexto.value; 
+    campoTexto.value = texto.toLowerCase();
+}
